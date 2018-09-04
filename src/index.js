206d66ab3d421340b9754d5e0cdc5cfd348df9e7
Daniel Brazel <dbrazel4@gmail.com>
2018-09-04 14:37:09 -0400

    added reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+
+const initState = {
+  level: 1
+}
+
+function reducer(state = initState, action){
+  return state
+}
+
+const store = createStore(reducer)
+
+console.log("store", store, store.getState());
+
+let action = {
+  type: "INCREMENT_LEVEL",
+  payload: 
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
