1e6563a4d5220d46a324c6b8da75bf80c88ad5bb
Daniel Brazel <dbrazel4@gmail.com>
2018-09-17 20:24:55 -0400

    Added buttons to App.js to manipulate store

--- src/App.js
+++ src/App.js
@@ -3,10 +3,32 @@ import './App.css';
 import { connect } from 'react-redux';
 
 class App extends Component {
+  increment = () => {
+    this.props.dispatch({
+      type: 'INCREMENT_LEVEL'
+    })
+  }
+
+  decerement = () => {
+    this.props.dispatch({
+      type: 'DECREMENT_LEVEL'
+    })
+  }
+
+  reset = () => {
+    this.props.dispatch({
+      type: 'SET_LEVEL',
+      payload: 1
+    })
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div className="App">
-
+        <button onClick={this.increment}>Click to Increment Level</button>
+        <button onClick={this.decerement}>Click to Decrement Level</button>
+        <button onClick={this.reset}>Click to Reset Level</button>
       </div>
     );
   }
