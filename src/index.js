27b47dcd1ddba1f3414cdf30c8d6beeb1353a8ab
Daniel Brazel <dbrazel4@gmail.com>
2018-09-21 22:38:55 -0400

    Moved redux components to separate files

--- src/index.js
+++ src/index.js
@@ -3,45 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import{INCREMENT_LEVEL, DECREMENT_LEVEL, SET_LEVEL} from './types.js';
+import reducer from './reducer.js';
+import store from './store.js'
 
-const initState = {
-  level: 1
-}
-
-function reducer(state = initState, action){
-  switch(action.type){
-    case INCREMENT_LEVEL:
-      return {...state, level: state.level + 1}
-    case DECREMENT_LEVEL:
-      return {...state, level: state.level - 1}
-    case SET_LEVEL:
-      return {...state, level: action.payload}
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer)
-
-console.log("store", store, store.getState());
-
-
-const dispatchWithLog = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    console.group("DISPATCH");
-    console.log('%c state before dispatch', 'color: red', store.getState())
-    const returnValue = rawDispatch(action);
-    console.log('%c state after dispatch', 'color: red', store.getState())
-    console.groupEnd();
-    return returnValue;
-  }
-}
-
-store.dispatch = dispatchWithLog(store)
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
